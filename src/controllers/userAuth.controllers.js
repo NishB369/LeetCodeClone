2768dabb227ba5067c159318700946f9b8bac745
nishb <nishbcodes@gmail.com>
2025-10-25 00:41:41 +0530

    User Auth - Login Controller, Router, Validator Added

--- src/controllers/userAuth.controllers.js
+++ src/controllers/userAuth.controllers.js
@@ -1,5 +1,5 @@
 const User = require("../models/user.js");
-const { validate } = require("../utils/validator.js");
+const { validate, validateLogin } = require("../utils/validator.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -28,4 +28,39 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    validateLogin(req.body);
+    const { emailId, password } = req.body;
+
+    if (!emailId || !password) {
+      throw new Error("Invalid Credntials!");
+    }
+
+    const user = await User.findOne({ emailId });
+
+    if (!user) {
+      throw new Error("User Not Found");
+    }
+
+    const matched = await bcrypt.compare(password, user?.password);
+
+    if (!matched) {
+      throw new Error("Invalid Credntials!");
+    }
+    const token = jwt.sign(
+      { _id: user._id, emailId: emailId },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+      }
+    );
+
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+    res.status(201).send("User Logged In Successfully");
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = { register, login };
