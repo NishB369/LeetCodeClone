f3c85abf422d8d56bd4f80d7c07040c5d639d359
nishb <nishbcodes@gmail.com>
2025-10-25 13:40:24 +0530

    Redis Client Configured and Connected

--- index.js
+++ index.js
@@ -2,19 +2,34 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const main = require("./src/config/db.js");
+const { redisClient } = require("./src/config/redis.js");
 const cookieParser = require("cookie-parser");
 const { authRouter } = require("./src/routes/userAuth.routes.js");
 
 app.use(express.json());
 app.use(cookieParser());
 
-main()
-  .then(async () => {
+const initialiseConnection = async () => {
+  try {
+    await Promise.all([main(), redisClient.connect()]);
+    console.log("DB Connected");
     app.listen(process.env.PORT, () => {
       console.log("Server listening at port number: " + process.env.PORT);
     });
-  })
-  .catch((err) => console.log("Error Occurred: " + err));
+  } catch (error) {
+    console.log("Error: ", error.message);
+  }
+};
+
+// main()
+//   .then(async () => {
+//     app.listen(process.env.PORT, () => {
+//       console.log("Server listening at port number: " + process.env.PORT);
+//     });
+//   })
+//   .catch((err) => console.log("Error Occurred: " + err));
+
+initialiseConnection();
 
 app.use("/api/v1/auth", authRouter);
 
