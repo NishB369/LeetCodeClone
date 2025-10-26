e716488325dbbbc417e525d14792ad3c4e3bd80a
nishb <nishbcodes@gmail.com>
2025-10-26 23:37:46 +0530

    Update and Delete Problem - Controllers and Routes Added

--- src/controllers/problemsCrud.controllers.js
+++ src/controllers/problemsCrud.controllers.js
@@ -6,17 +6,7 @@ const {
 } = require("../utils/problemsUtility.js");
 
 const createProblem = async (req, res) => {
-  const {
-    title,
-    description,
-    difficulty,
-    tags,
-    visibleTestCases,
-    hiddenTestCases,
-    startCode,
-    referenceSolutions,
-    problemCreator,
-  } = req.body;
+  const { visibleTestCases, referenceSolutions } = req.body;
 
   try {
     for (const elem of referenceSolutions) {
@@ -48,10 +38,80 @@ const createProblem = async (req, res) => {
       problemCreator: req.result._id,
     });
 
-    res.status(201).send("Problem Created Successfully!");
+    res.status(201).send("Problem Created Successfully!", userProblem);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
-module.exports = { createProblem };
+const updateProblem = async (req, res) => {
+  const { id } = req.params;
+  const { visibleTestCases, referenceSolutions } = req.body;
+
+  try {
+    if (!id) {
+      return res.status(401).send("ID Field Not Passed");
+    }
+
+    const problem = await Problem.findById(id);
+    if (!problem) {
+      return res.status(404).send("Problem Not Found");
+    }
+
+    for (const elem of referenceSolutions) {
+      const { language, completeCode } = elem;
+      const languageId = getLanguageById(language);
+
+      const submissions = visibleTestCases.map((testcase) => ({
+        source_code: completeCode,
+        language_id: languageId,
+        stdin: testcase.input,
+        expected_output: testcase.output,
+      }));
+
+      const submitResult = await submitBatch(submissions);
+
+      const resultToken = submitResult.map((value) => value.token);
+
+      const testResult = await submitToken(resultToken);
+
+      for (const test of testResult) {
+        if (test.status_id !== 3) {
+          return res.status(400).send("Error Occurred");
+        }
+      }
+    }
+
+    const newProblem = await Problem.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { runValidators: true, new: true }
+    );
+
+    console.log(newProblem);
+    res.status(200).send("Problem Updated Successfully", newProblem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteProblem = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!id) {
+      return res.status(401).send("ID Field Not Passed");
+    }
+
+    const problem = await Problem.findById(id);
+    if (!problem) {
+      return res.status(404).send("Problem Not Found");
+    }
+
+    const deletedProblem = await Problem.findOneAndDelete(id);
+    res.status(200).send("Problem Deleted Successfully", deletedProblem);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { createProblem, updateProblem, deleteProblem };
--- src/routes/problemCRUD.routes.js
+++ src/routes/problemCRUD.routes.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const { adminMiddleware } = require("../middlewares/admin.middleware.js");
-const { createProblem } = require("../controllers/problemsCrud.controllers.js");
+const { userMiddleware } = require("../middlewares/user.middlewares.js");
+const {
+  createProblem,
+  updateProblem,
+  deleteProblem,
+} = require("../controllers/problemsCrud.controllers.js");
 
 const problemRouter = express.Router();
 
 // Admin Protected Routes
 problemRouter.post("/create", adminMiddleware, createProblem);
-// problemRouter.patch("/:id", updateProblem);
-// problemRouter.delete("/:id", deleteProblem);
+problemRouter.patch("/update/:id", adminMiddleware, updateProblem);
+problemRouter.delete("/delete/:id", adminMiddleware, deleteProblem);
 
-// // Open Routes
-// problemRouter.get("/:id", getProblemById);
-// problemRouter.get("/", getAllProblems);
-// problemRouter.get("/user", getAllSolvedProblemsByUser);
+// Open Routes
+// problemRouter.get("/problemById/:id", userMiddleware, getProblemById);
+// problemRouter.get("/getAllProblems", userMiddleware, getAllProblems);
+// problemRouter.get(
+//   "/problemsSolvedByUser",
+//   userMiddleware,
+//   getAllSolvedProblemsByUser
+// );
 
 module.exports = { problemRouter };
